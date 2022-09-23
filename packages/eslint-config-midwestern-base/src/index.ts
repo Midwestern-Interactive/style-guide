cb220d3bb1291f5f27179ac3bc38834266afc193
Christian Garrison <cg@buildmidwestern.com>
2022-09-23 08:55:10 -0500

    feat: create source entry point file

---
+++ packages/eslint-config-midwestern-base/src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview Midwestern's Base TypeScript ESLint Configuration
+ * @author Midwestern Interactive
+ */
+
+const baseConfig = {
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {},
+};
+
+export default baseConfig;
