02694881fe5a19453b8aa927fafb725f4d9f6996
Christian Garrison <cg@buildmidwestern.com>
2022-09-26 23:33:20 -0500

    fix: update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  projects: ['<rootDir>/packages/*/jest.config.ts'],
 };
 
 export default config;
