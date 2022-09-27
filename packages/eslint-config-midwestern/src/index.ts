2c6405545f7828bb745f94fc0ad6762bea4d50a1
Christian Garrison <cg@buildmidwestern.com>
2022-09-27 01:09:58 -0500

    chore: work on build compilation

--- packages/eslint-config-midwestern/src/index.ts
+++ packages/eslint-config-midwestern/src/index.ts
@@ -3,7 +3,10 @@
  * @author Midwestern Interactive
  */
 
-const baseConfig = {
+// @rushstack/eslint-patch a patch that improves how ESLint loads plugins
+import '@rushstack/eslint-patch/modern-module-resolution';
+
+const config = {
   extends: [
     'eslint:recommended',
     'plugin:import/recommended',
@@ -18,7 +21,19 @@ const baseConfig = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'import', 'prettier'],
-  rules: {},
+  rules: {
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
 
-export default baseConfig;
+export default config;
